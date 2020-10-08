67aa0f24b4ac1f53faa6f5c13bc261724a421f1b
Imag0136 <Imag0136@DESKTOP>
2020-10-08 14:38:34 +0700

    Рефакторинг

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace Lab0
 {
@@ -19,13 +12,13 @@ namespace Lab0
         Graphics g;
         Bitmap img;
         Neuron neuron = new Neuron();
-        
+
         public Form1()
         {
             InitializeComponent();
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             p.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
-            
+
             neuron.Learn(img);
         }
 
